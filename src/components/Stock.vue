d55106cffcc100b14684094b6b5f871731c0282d
markhorsman <mark.horsman@tam-one.nl>
2019-07-28 12:29:02 +0200

    Stock component test ready

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,8 +1,62 @@
 <template>
   <div>
-    <br />
-    <br />
+    <q-select
+      filled
+      clearable
+      v-model="group"
+      use-input
+      hide-selected
+      fill-input
+      input-debounce="0"
+      :options="groupOptions"
+      @filter="filterGroups"
+      @input="onRequest({
+      pagination,
+      filter
+    })"
+      @remove="onRequest({
+      pagination,
+      filter
+    })"
+      class="float-left"
+      label="Hoofdgroep"
+      style="width: 50%"
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">Geen resultaten</q-item-section>
+        </q-item>
+      </template>
+    </q-select>
 
+    <q-select
+      filled
+      clearable
+      v-model="subgroup"
+      use-input
+      hide-selected
+      fill-input
+      input-debounce="0"
+      :options="subgroupOptions"
+      @filter="filterSubgroups"
+      @input="onRequest({
+      pagination,
+      filter
+    })"
+      @remove="onRequest({
+      pagination,
+      filter
+    })"
+      label="Subgroep"
+      float-left
+      style="width: 50%; padding-bottom: 32px"
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">Geen resultaten</q-item-section>
+        </q-item>
+      </template>
+    </q-select>
     <q-table
       class="sticky-header-table"
       title="Voorraad"
@@ -16,7 +70,13 @@
       @request="onRequest"
     >
       <template v-slot:top-right>
-        <q-input borderless dense debounce="300" v-model="filter" placeholder="Zoek op artikel">
+        <q-input
+          borderless
+          dense
+          debounce="500"
+          v-model="filter"
+          placeholder="Zoek op artikelnummer"
+        >
           <template v-slot:append>
             <q-icon name="search" />
           </template>
@@ -114,8 +174,14 @@ export default {
       tableData: [],
       filter: "",
       loading: false,
-      groups: [],
-      subgroups: []
+      groups: {
+        main: [],
+        sub: []
+      },
+      groupOptions: [],
+      subgroupOptions: [],
+      group: null,
+      subgroup: null
     };
   },
 
@@ -142,15 +208,21 @@ export default {
       // calculate starting row of data
       let startRow = (page - 1) * rowsPerPage;
 
+      const buildFilter = () =>
+        `CURRDEPOT eq '${this.$store.state.user.DEPOT}'${
+          filter ? ` and startswith(ITEMNO, '${filter}')` : ``
+        }
+        ${this.group ? ` and PGROUP eq '${this.group.value}'` : ``}
+        ${this.subgroup ? ` and GRPCODE eq '${this.subgroup.value}'` : ``}
+        `;
+
       this.$api
         .get(
           `${this.$config.api_base_url}stock?api_key=${
             this.$store.state.api_key
           }&$top=${rowsPerPage}&$skip=${startRow}&$inlinecount=allpages${
             sortBy ? `&$orderby=${sortBy} ${descending ? `desc` : `asc`}` : ``
-          }&$filter=CURRDEPOT eq '${
-            this.$store.state.user.DEPOT
-          }'&fields=CURRDEPOT,PGROUP,GRPCODE,ITEMNO,DESC1,DESC2,DESC3,STATUS`
+          }&$filter=${buildFilter()}&fields=CURRDEPOT,PGROUP,GRPCODE,ITEMNO,DESC1,DESC2,DESC3,STATUS`
         )
         .then(res => {
           this.pagination.page = page;
@@ -176,35 +248,62 @@ export default {
       ])
         .then(res => {
           const all = res[0].data;
-          this.subgroups = res[1].data;
-          this.groups = this.subgroups.reduce((acc, grp) => {
-            if (!acc.find(p => p.CODE === grp.PGROUP)) {
+          this.groups.main = res[1].data.reduce((acc, grp) => {
+            if (!acc.find(p => p.value === grp.PGROUP)) {
               const group = all.find(p => p.CODE === grp.PGROUP);
-              if (group) acc.push(group);
+              if (group) acc.push({ label: group.NAME, value: group.CODE });
             }
             return acc;
           }, []);
+
+          this.groups.sub = res[1].data.reduce((acc, grp) => {
+            acc.push({ label: grp.NAME, value: grp.CODE, pgroup: grp.PGROUP });
+            return acc;
+          }, []);
+
+          this.groupOptions = this.groups.main;
+          this.subgroupOptions = this.groups.sub;
         })
         .catch(() => {});
+    },
+
+    filterGroups(val, update, abort) {
+      update(() => {
+        const needle = val.toLowerCase();
+        this.groupOptions = this.groups.main.filter(
+          v => v.label.toLowerCase().indexOf(needle) > -1
+        );
+      });
+    },
+
+    filterSubgroups(val, update, abort) {
+      update(() => {
+        const needle = val.toLowerCase();
+        this.subgroupOptions = this.groups.sub.filter(
+          v => v.label.toLowerCase().indexOf(needle) > -1 && (this.group ? v.pgroup === this.group.value : true)
+        );
+      });
     }
   }
 };
 </script>
 
 <style lang="stylus">
-.sticky-header-table
+.sticky-header-table {
   /* max height is important */
-  .q-table__middle
-    max-height 500px
+  .q-table__middle {
+    max-height: 500px;
+  }
 
-  .q-table__top,
-  .q-table__bottom,
-  thead tr:first-child th /* bg color is important for th; just specify one */
-    background-color #ffc20e
+  .q-table__top, .q-table__bottom, thead tr:first-child th {
+    background-color: #ffc20e;
+  }
 
-  thead tr:first-child th
-    position sticky
-    top 0
-    opacity 1
-    z-index 1
+  thead tr:first-child th {
+    position: sticky;
+    top: 0;
+    opacity: 1;
+    z-index: 1;
+  }
+}
 </style>
