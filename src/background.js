70dfa2f1ca2b0e8b550648d6a677ea9b291d66ed
markhorsman <mark.horsman@tam-one.nl>
2019-08-04 15:35:15 +0200

    Version bump + native dialog for update notification

--- src/background.js
+++ src/background.js
@@ -1,53 +1,45 @@
 'use strict'
 
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, dialog } from 'electron'
 import {
   createProtocol,
   installVueDevtools
 } from 'vue-cli-plugin-electron-builder/lib'
 
-// const log = require('electron-log')
+const log = require('electron-log')
 const { autoUpdater } = require("electron-updater")
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// autoUpdater.logger = log
-// autoUpdater.logger.transports.file.level = 'debug'
-// log.info('App starting...')
+autoUpdater.logger = log
+autoUpdater.logger.transports.file.level = 'info'
+log.info('App starting...')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-// function sendStatusToWindow(text) {
-//   log.info(text);
-//   win.webContents.send('message', text);
-// }
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
 
 // Standard scheme must be registered before the app is ready
 protocol.registerStandardSchemes(['app'], { secure: true })
 
-// autoUpdater.on('checking-for-update', () => {
-//   sendStatusToWindow('Checking for update...');
-// })
-// autoUpdater.on('update-available', (info) => {
-//   sendStatusToWindow('Update available.');
-// })
-// autoUpdater.on('update-not-available', (info) => {
-//   sendStatusToWindow('Update not available.');
-// })
-// autoUpdater.on('error', (err) => {
-//   sendStatusToWindow('Error in auto-updater. ' + err);
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-//   let log_message = "Download speed: " + progressObj.bytesPerSecond;
-//   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-//   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-//   sendStatusToWindow(log_message);
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   sendStatusToWindow('Update downloaded');
-// });
+autoUpdater.on('update-downloaded', (info) => {
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Herstarten', 'Nu niet'],
+    title: 'App update',
+    message: 'Nieuwe update beschikbaar!',
+    detail: `Er is een nieuwe versie (${info.releaseName}) gedownload. Herstart de app om de nieuwste versie te gebruiken.`
+  }
+
+  dialog.showMessageBox(dialogOpts, (response) => {
+    if (response === 0) autoUpdater.quitAndInstall()
+  })
+});
 
 function createWindow() {
   // Create the browser window.
@@ -96,9 +88,7 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  // autoUpdater.checkForUpdatesAndNotify()
   createWindow()
-  autoUpdater.checkForUpdatesAndNotify()
 
   await installVueDevtools()
   // if (isDevelopment && !process.env.IS_TEST) {
@@ -106,6 +96,9 @@ app.on('ready', async () => {
   //   await installVueDevtools()
   // }
 
+  setInterval(() => {
+    autoUpdater.checkForUpdates()
+  }, 60000)
 })
 
 // Exit cleanly on request from parent process in development mode.
